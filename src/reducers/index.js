e7ecb74b605e1a2aed20d710ce678a5adcca7f38
Marbella <joo.luiz.98@outlook.com>
2019-09-01 17:47:32 -0300

    Refactoring and travel mode

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from "redux";
 import map from "./map";
-import directions from "./directions";
 
-export default combineReducers({ map, directions });
+export default combineReducers({ map });
