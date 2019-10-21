eb2be63466338598ec23d08975b2c8f713bf77f2
Marbella <joo.luiz.98@outlook.com>
2019-10-20 22:09:11 -0300

    Changing app name from AgoraVai to RotaSegura

--- android/app/src/main/java/com/RotaSegura/MainActivity.java
+++ android/app/src/main/java/com/RotaSegura/MainActivity.java
@@ -1,4 +1,4 @@
-package com.agoravai;
+package com.RotaSegura;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "agoraVai";
+        return "RotaSegura";
     }
 
     @Override
