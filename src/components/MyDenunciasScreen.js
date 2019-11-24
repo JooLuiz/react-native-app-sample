667e525cafeaeab580068a29a264a74ae6988ce1
Marbella <joo.luiz.98@outlook.com>
2019-11-24 16:30:31 -0300

    Adjusting myplaces and mydenuncias screens

--- src/components/MyDenunciasScreen.js
+++ src/components/MyDenunciasScreen.js
@@ -13,6 +13,7 @@ import { getDenuncias } from "../actions/denuncias";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import EmptyList from "./common/EmptyList";
 import GoBackButton from "./common/GoBackButton";
+import { List } from "react-native-paper";
 
 class MyDenunciasScreen extends React.Component {
   componentWillMount() {
@@ -24,7 +25,7 @@ class MyDenunciasScreen extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={styles.container}>
         <GoBackButton
           navigation={this.props.navigation}
           title="Minhas Denúncias"
@@ -35,34 +36,32 @@ class MyDenunciasScreen extends React.Component {
             <EmptyList text="Você ainda não possui nenhuma denuncia cadastrada" />
           }
           renderItem={({ item }) => (
-            <TouchableHighlight>
-              <View>
-                <View style={styles.listItens}>
-                  <FontAwesomeIcon
-                    icon={
-                      this.props.denuncias.filter(d => d.id == item.denuncia)[0]
-                        .icone
-                    }
-                    color={"black"}
-                    size={30}
+            <View>
+              <List.Item
+                title={
+                  this.props.denuncias.filter(d => d.id == item.denuncia)[0]
+                    .descricao
+                }
+                description={item.comentario}
+                left={() => (
+                  <List.Icon
+                    icon={() => (
+                      <FontAwesomeIcon
+                        icon={
+                          this.props.denuncias.filter(
+                            d => d.id == item.denuncia
+                          )[0].icone
+                        }
+                        color={"black"}
+                        size={30}
+                      />
+                    )}
                   />
-                  <View style={styles.itemName}>
-                    <Text>
-                      Denuncia:
-                      {
-                        this.props.denuncias.filter(
-                          d => d.id == item.denuncia
-                        )[0].descricao
-                      }
-                    </Text>
-                    <Text>Comentário:{item.comentario}</Text>
-                  </View>
-                </View>
-              </View>
-            </TouchableHighlight>
+                )}
+              />
+            </View>
           )}
         />
-        <BottomButtons navigation={this.props.navigation} />
       </View>
     );
   }
@@ -75,30 +74,9 @@ const mapStateToProps = state => ({
 });
 
 const styles = StyleSheet.create({
-  itemName: {
-    height: Dimensions.get("window").height * 0.07,
-    width: Dimensions.get("window").width,
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
-    left: Dimensions.get("window").width * 0.03
-  },
-  listItens: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height * 0.1,
-    left: Dimensions.get("window").height * 0.025
-  },
-  nameText: {
-    fontSize: 15,
-    color: "black"
-  },
-  division: {
-    alignSelf: "center",
-    width: Dimensions.get("window").width * 0.9,
-    borderBottomColor: "grey",
-    borderBottomWidth: 1
+  container: {
+    marginTop: 0,
+    backgroundColor: "#ffffff"
   }
 });
 
--- src/components/MyPlacesScreen.js
+++ src/components/MyPlacesScreen.js
@@ -15,6 +15,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { getEnderecoUsuario } from "../actions/enderecosUsuario";
 import GoBackButton from "./common/GoBackButton";
 import { getPlace, setOrigin } from "../actions/map";
+import { List } from "react-native-paper";
 
 class MyPlacesScreen extends React.Component {
   componentWillMount() {
@@ -26,7 +27,7 @@ class MyPlacesScreen extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View styles={styles.container}>
         <GoBackButton navigation={this.props.navigation} title="Meus Locais" />
         <FlatList
           data={this.props.enderecosUsuario}
@@ -34,28 +35,28 @@ class MyPlacesScreen extends React.Component {
             <EmptyList text="Você ainda não possui nenhum endereço cadastrado" />
           }
           renderItem={({ item }) => (
-            <TouchableHighlight
-              onPress={() => {
-                let coordinates = {
-                  latitude: item.latitude,
-                  longitude: item.longitude
-                };
-                this.props.getPlace(coordinates, "coordinates");
-                this.props.setOrigin();
-                this.props.navigation.navigate("Mapa");
-              }}
-            >
-              <View>
-                <View style={styles.listItens}>
-                  <FontAwesomeIcon icon="map-marker-alt" size={30} />
-                  <View style={styles.itemName}>
-                    <Text style={styles.nameText}>{item.nome}</Text>
-                    <Text style={styles.addressText}>{item.endereco}</Text>
-                  </View>
-                </View>
-                <View style={styles.division} />
-              </View>
-            </TouchableHighlight>
+            <View>
+              <List.Item
+                title={item.nome}
+                description={item.endereco}
+                onPress={() => {
+                  let coordinates = {
+                    latitude: item.latitude,
+                    longitude: item.longitude
+                  };
+                  this.props.getPlace(coordinates, "coordinates");
+                  this.props.setOrigin();
+                  this.props.navigation.navigate("Mapa");
+                }}
+                left={() => (
+                  <List.Icon
+                    icon={() => (
+                      <FontAwesomeIcon icon="map-marker-alt" size={30} />
+                    )}
+                  />
+                )}
+              />
+            </View>
           )}
         />
         <View style={styles.addPlaceBottomButtom}>
@@ -69,56 +70,28 @@ class MyPlacesScreen extends React.Component {
             </View>
           </TouchableOpacity>
         </View>
-        <BottomButtons navigation={this.props.navigation} />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  itemName: {
-    height: Dimensions.get("window").height * 0.07,
-    width: Dimensions.get("window").width,
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
-    left: Dimensions.get("window").width * 0.03
-  },
-  listItens: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height * 0.1,
-    left: Dimensions.get("window").height * 0.025
+  container: {
+    marginTop: 0,
+    backgroundColor: "#ffffff"
   },
   addPlaceBottomButtom: {
-    flex: 1,
     position: "absolute",
     bottom: Dimensions.get("window").height * 0.15,
     right: Dimensions.get("window").width * 0.12
   },
-  nameText: {
-    fontSize: 15,
-    color: "black"
-  },
-  addressText: {
-    fontSize: 12,
-    color: "grey"
-  },
   circle: {
-    flex: 2,
     alignItems: "center",
     justifyContent: "center",
     height: Dimensions.get("window").width * 0.17,
     width: Dimensions.get("window").width * 0.17,
     borderRadius: 400,
     backgroundColor: "#3B4859"
-  },
-  division: {
-    alignSelf: "center",
-    width: Dimensions.get("window").width * 0.9,
-    borderBottomColor: "grey",
-    borderBottomWidth: 1
   }
 });
 
